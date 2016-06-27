1056abcdd543ad4d31754b2053d0fdbe27ac0554
Hengki Sihombing <hengki.sihombing@gmail.com>
2016-06-27 11:46:01 +0700

    add addthis share

--- single.php
+++ single.php
@@ -66,7 +66,8 @@
 
                                     }
                                 ?>
-
+                                <div class="addthis_sharing_toolbox"></div>
+                                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-576cf311b39ae995"></script>
                             <?php endwhile; // end of the loop. ?>
 
                         <?php else : ?>
