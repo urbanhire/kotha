2267fb214c273660c6dac755b2e728db43a3267b
Hengki Sihombing <hengki.sihombing@gmail.com>
2016-10-25 17:56:33 +0700

    add facebook pixel

--- header.php
+++ header.php
@@ -24,6 +24,24 @@
 	<link rel="dns-prefetch" href="//a.disquscdn.com">
 	<link rel="dns-prefetch" href="//api.pinterest.com">
 	<link rel="dns-prefetch" href="//www.linkedin.com">
+
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '259164591143162');
+		fbq('track', 'Search');
+		fbq('track', 'Lead');
+		fbq('track', "PageView");</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=259164591143162&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
