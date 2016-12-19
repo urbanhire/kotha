5cc900cc76d46c945737a840f89ddd24f5e46143
Hengki Sihombing <hengki.sihombing@gmail.com>
2016-12-19 18:22:15 +0700

    add sidebar banner banner

--- sidebar.php
+++ sidebar.php
@@ -3,6 +3,11 @@
 
     } else { ?>
         <div class="col-md-4">
+            <dir>
+              <a href="https://www.urbanhire.com/blog/ask-urbanhire/" target="_blank">
+                <img src="https://lh5.googleusercontent.com/6P4IgflclUR0oSyNqSn6TiI8xa4DpeuHuG7zTLD2FTawBZ6hbGgUA_8NGrzuCNf_f3L62oh_Qw=w1024" style="width: 100%; max-width: 100%">
+              </a>
+            </dir>
             <div class="primary-sidebar widget-area" role="complementary">
                 <?php dynamic_sidebar('blog-sidebar'); ?>
             </div>
