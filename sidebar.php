ca532022b5e157f48ceb6ee0850cebd30a4d7044
Hengki Sihombing <hengki.sihombing@gmail.com>
2016-12-19 18:24:59 +0700

    fixing sidebar style

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 
     } else { ?>
         <div class="col-md-4">
-            <dir>
+            <dir style="margin: 0 0 10px 0; padding: 0">
               <a href="https://www.urbanhire.com/blog/ask-urbanhire/" target="_blank">
                 <img src="https://lh5.googleusercontent.com/6P4IgflclUR0oSyNqSn6TiI8xa4DpeuHuG7zTLD2FTawBZ6hbGgUA_8NGrzuCNf_f3L62oh_Qw=w1024" style="width: 100%; max-width: 100%">
               </a>
