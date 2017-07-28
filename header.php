79bc4be859e436e0a53084708364e5ade415d7d7
Hengki Sihombing <hengki.sihombing@gmail.com>
2017-07-28 14:37:42 +0700

    fixing GTM tag placement position

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TWFJJF');
+	</script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -48,10 +51,6 @@
 	<noscript>
 	  <iframe ssrc="//www.googletagmanager.com/ns.html?id=GTM-TWFJJF" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 	</noscript>
-	<script>
-	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TWFJJF');
-	</script>
 <?php // if(!get_theme_mod('kotha_preloader') ): ?>
     <!-- <div id="st-preloader">
         <div id="pre-status">
